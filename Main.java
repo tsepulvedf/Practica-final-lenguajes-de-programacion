0ad7b3997eed85e57c500265dd5e75c43672ba20
tsepulvedf <85259415+tsepulvedf@users.noreply.github.com>
2022-06-01 03:03:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
 
-  public static void main(String[]args){
+  public static void main(String[]args) throws IOException {
     var sn = new Scanner(System.in);
     var salir = false;
     var opcion = 0;
